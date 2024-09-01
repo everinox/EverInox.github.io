70c2a7a2524aed6b51046fd312759d7e426731e4
jashif <jashif@live.com>
2024-09-01 19:39:28 +0200

    chore: update bg

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,68 +1,70 @@
-import React, { Component } from "react";
+import React from "react";
 import ParticlesBg from "particles-bg";
 import Fade from "react-reveal";
 import Zmage from "react-zmage";
-class Header extends Component {
-  render() {
-    if (!this.props.data) return null;
 
-    const name = this.props.data.name;
-    const description = this.props.data.description;
-    const image = this.props.data.image;
-    return (
-      <header id="home">
-        <ParticlesBg type="circle" bg={true} />
+const Header = ({ data }) => {
+  if (!data) return null;
 
-        <nav id="nav-wrap">
-          <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
-            Show navigation
-          </a>
-          <a className="mobile-btn" href="#home" title="Hide navigation">
-            Hide navigation
-          </a>
+  const { name, description, image, image2,bio2 } = data;
 
-          <ul id="nav" className="nav">
-            <li className="current">
-              <a className="smoothscroll" href="#home">
-                Home
-              </a>
-            </li>
+  return (
+    <header id="home">
+      <ParticlesBg type="circle" bg={true} />
 
-            <li>
-              <a className="smoothscroll" href="#about">
-                About
-              </a>
-            </li>
+      <nav id="nav-wrap">
+        <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
+          Show navigation
+        </a>
+        <a className="mobile-btn" href="#home" title="Hide navigation">
+          Hide navigation
+        </a>
 
-            <li>
-              <a className="smoothscroll" href="#contact">
-                Contact
-              </a>
-            </li>
-          </ul>
-        </nav>
+        <ul id="nav" className="nav">
+          <li className="current">
+            <a className="smoothscroll" href="#home">
+              Home
+            </a>
+          </li>
+          <li>
+            <a className="smoothscroll" href="#about">
+              About
+            </a>
+          </li>
+          <li>
+            <a className="smoothscroll" href="#contact">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </nav>
 
-        <div className="row banner">
-          <div className="banner-text">
-            <Fade bottom>
-              <Zmage alt={name} src={`images/${image}`} />
-            </Fade>
-            <Fade bottom duration={1200}>
-              <h3>{description}.</h3>
-            </Fade>
+      <div className="row banner">
+        <div className="banner-text">
+          <Fade bottom>
+            <Zmage alt={name} src={`images/${image2}`} />
+          </Fade>
+          <Fade bottom duration={1200}>
+            <h3>{bio2}.</h3>
+          </Fade>
+          <Fade bottom>
+            <Zmage alt={name} src={`images/${image}`} />
+          </Fade>
+          <Fade bottom duration={1200}>
+            <h3>{description}.</h3>
+          </Fade>
 
-            <hr />
-          </div>
+          <hr />
         </div>
+      </div>
 
-        <p className="scrolldown">
-          <a className="smoothscroll" href="#about">
-            <i className="icon-down-circle"></i>
-          </a>
-        </p>
-      </header>
-    );
-  }
-}
+      <p className="scrolldown">
+        <a className="smoothscroll" href="#about">
+          <i className="icon-down-circle"></i>
+        </a>
+      </p>
+    </header>
+  );
+};
 
 export default Header;

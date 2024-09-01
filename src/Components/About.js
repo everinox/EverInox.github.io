f6eaab7454ac040ac6a4ff628e46c5b3f657e73f
jashif <jashif@live.com>
2024-09-01 19:22:05 +0200

    chore: add ca address

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,45 +1,36 @@
-import React, { Component } from "react";
+import React from "react";
 import Fade from "react-reveal";
 
-class About extends Component {
-  render() {
-    if (!this.props.data) return null;
+const About = ({ data }) => {
+  if (!data) return null;
 
-    const name = this.props.data.name;
-    // const profilepic = "images/" + this.props.data.image;
-    const bio = this.props.data.bio;
-    const street = this.props.data.address.street;
-    const city = this.props.data.address.city;
-    const state = this.props.data.address.state;
-    const zip = this.props.data.address.zip;
-    const phone = this.props.data.phone;
-    const email = this.props.data.email;
+  const {  bio, addresses, phone, email } = data;
 
-    return (
-      <section id="about">
-        <Fade duration={1000}>
-          <div className="row">
-            <div className="three columns">
-              {/* <img
-                className="profile-pic"
-                src={profilepic}
-                alt="Nordic Giant Profile Pic"
-              /> */}
-            </div>
-            <div className="nine columns main-col">
-              <h2>About Us</h2>
-
-              <p>{bio}</p>
-              <div className="row">
-                <div className="columns contact-details">
-                  <h2>Contact Details</h2>
+  return (
+    <section id="about">
+      <Fade duration={1000}>
+        <div className="row">
+          <div className="three columns">
+            {/* You can uncomment and use this if you want to show a profile picture */}
+            {/* <img
+              className="profile-pic"
+              src={"images/" + data.image}
+              alt="Profile Pic"
+            /> */}
+          </div>
+          <div className="nine columns main-col">
+            <h2>About Us</h2>
+            <p>{bio}</p>
+            <div className="row">
+              {/* Map through the addresses array to render multiple columns */}
+              {addresses.map((address, index) => (
+                <div key={index} className="columns contact-details">
                   <p className="address">
-                    <span>{name}</span>
-                    <br />
+                    <h2>{address.name}</h2>
                     <span>
-                      {street}
+                      {address.street}
                       <br />
-                      {city} {state}, {zip}
+                      {address.city} {address.state}, {address.zip}
                     </span>
                     <br />
                     <span>{phone}</span>
@@ -47,14 +38,13 @@ class About extends Component {
                     <span>{email}</span>
                   </p>
                 </div>
-                <div className="columns download"></div>
-              </div>
+              ))}
             </div>
           </div>
-        </Fade>
-      </section>
-    );
-  }
-}
+        </div>
+      </Fade>
+    </section>
+  );
+};
 
 export default About;
